74687dc53b4237aebadf6259597a18de0015a946
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2024-03-04 22:22:08 +0900

    ローカルフックも存在すれば呼び出し

--- utils/exec-local-hooks.sh
+++ utils/exec-local-hooks.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+script_dir=$(dirname "$0")
+
+source  "${script_dir}/git-info.rc"
+
+git_root=$(get-git-root)
+hook_name=$(basename "$0")
+local_hook="${git_root}/.git/hooks/${hook_name}"
+
+if [[ -e "${local_hook}" ]] ; then
+    source  "${local_hook}"
+fi
